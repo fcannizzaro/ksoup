8e81a0b8d4cdc97b218f2cde65d55b5c03fa7f2a
Francesco Cannizzaro <francisthemoderator@gmail.com>
2017-08-26 20:37:39 +0200

    fix bindAttr

--- library/src/main/java/com/fcannizzaro/ksoup/delegates/bindAttr.kt
+++ library/src/main/java/com/fcannizzaro/ksoup/delegates/bindAttr.kt
@@ -1,6 +1,7 @@
 package com.fcannizzaro.ksoup.delegates
 
 import com.fcannizzaro.ksoup.IKsoup
+import com.fcannizzaro.ksoup.KAttr
 import com.fcannizzaro.ksoup.util.extractAttr
 import kotlin.reflect.KProperty
 
@@ -9,6 +10,8 @@ import kotlin.reflect.KProperty
  */
 
 class bindAttr(private var query: String, private val attr: String, private val trim: Boolean = true) {
+
+    constructor(query: String, attr: KAttr, trim: Boolean = true) : this(query, attr.attr, trim)
 
     operator fun getValue(ref: IKsoup, property: KProperty<*>): String? = extractAttr(ref.element, query, attr, trim)
 
